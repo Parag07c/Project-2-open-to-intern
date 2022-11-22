f7f299e3a22e1bbd4f160f7d1fbd9dc8b24596cf
Parag07c <choudharyparag001@gmail.com>
2022-11-22 13:40:01 +0530

    add router all

--- src/router/router.js
+++ src/router/router.js
@@ -8,6 +8,12 @@ router.post('/functionup/colleges',collegeController.createCollege)
 router.post('/functionup/interns',internController.createIntern)
 router.get('/functionup/collegeDetails',collegeController.getDetail)
 
+router.all("/*",function(req,res){
+    res.status(400).send({uparDekho:"Endpoint galath dala hai tune"
+}
+)
+}
+)
 
 
 module.exports=router;
