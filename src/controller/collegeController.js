f697b1f4019e486c285f5476a71a3757585a633d
Parag07c <choudharyparag001@gmail.com>
2022-11-24 11:41:11 +0530

    few changes

--- src/controller/collegeController.js
+++ src/controller/collegeController.js
@@ -29,41 +29,42 @@ const createCollege= async function(req,res){
         let data =req.body
         let {name,fullName,logoLink}=data
         if (Object.keys(data).length == 0) {
-            return res.status(400).send({ status: false, msg: "Data is required for college Creation" });
+            return res.status(400).send({ status: false, message: "Data is required for college Creation" });
         }
         if(!isValid(name)){
-            return res.status(400).send({ status: false, msg: "Name is required" });
+            return res.status(400).send({ status: false, message: "Name is required" });
         }   
         data.name=data.name.toLowerCase()
         name=data.name.trim()
         if(!isValidShortName(name)){
-            return res.status(400).send({ status: false, msg: "Name should be contain alphabets only" });
+            return res.status(400).send({ status: false, message: "Name should be contain alphabets only" });
 
         }
         let valid =await collegeModel.findOne({name:name, isDeleted:false})
         if(valid){
-            return res.status(400).send({status:false,msg:"Name already exist"})
+            return res.status(400).send({status:false,message:"Name already exist"})
         }
         if(!isValid(fullName)){
-            return res.status(400).send({ status: false, msg: "fullName is required" });
+            return res.status(400).send({ status: false, message: "fullName is required" });
         }   
         if(!isValidFullName(fullName)){
-            return res.status(400).send({ status: false, msg: "fullName should be contain alphabets only" });
+            return res.status(400).send({ status: false, message: "fullName should be contain alphabets only" });
 
         }
-        if(!logoLink) return res.status(400).send({status:false,msg:"logo link is required"})
+        if(!logoLink) return res.status(400).send({status:false,message:"logo link is required"})
         logoLink=data.logoLink.trim()
+        
         if(!isValidLink(logoLink)){
-            return res.status(400).send({ status: false, msg: "Please enter a valid logoLink" });
+            return res.status(400).send({ status: false, message: "Please enter a valid logoLink" });
 
         }
         const creatData= await collegeModel.create(data)
        
-        return res.status(201).send({status:true,msg:creatData})
+        return res.status(201).send({status:true,data:creatData})
 
 
     } catch (error) {
-        return res.status(500).send({status:false,msg:error.message})
+        return res.status(500).send({status:false,message:error.message})
     }
 }
 
@@ -83,12 +84,12 @@ const getDetail=async function(req,res){
         let ColId = collegeDetail["_id"]
         let Interns = await internModel.find({ collegeId: ColId ,isDeleted:false}).select({name:1,email:1,mobile:1})
         
-        if(Interns.length==0) return res.status(404).send({status:false,msg:"Interns not found"})
+        if(Interns.length==0) return res.status(404).send({status:false,message:"Interns not found"})
         let data={name:collegeDetail.name,fullName:collegeDetail.fullName,logoLink:collegeDetail.logoLink,interns:Interns}
         return res.status(200).send({status:true,data:data})
     }
     catch (error) {
-        return res.status(500).send({status:false,msg:error.message})
+        return res.status(500).send({status:false,message:error.message})
         
     }
 }
--- src/controller/internController.js
+++ src/controller/internController.js
@@ -25,38 +25,38 @@ const isValidMobile = function (mobile) {
 const createIntern = async function (req, res) {
     try {
         const data = req.body
-        if (Object.keys(data).length == 0) return res.status(404).send({ status: false, msg: "Please provide Intern Details." })
+        if (Object.keys(data).length == 0) return res.status(400).send({ status: false, message: "Please provide Intern Details." })
         let { name, email, mobile, collegeName } = data
         //edge cases
-        if (!isValid(name)) return res.status(400).send({ status: false, msg: "Intern name must be required." })
-        if (!isValidName(name)) return res.status(400).send({ status: false, msg: "Intern name must consist of only leters." })
+        if (!isValid(name)) return res.status(400).send({ status: false, message: "Intern name must be required." })
+        if (!isValidName(name)) return res.status(400).send({ status: false, message: "Intern name must consist of only leters." })
 
-        if (!isValid(mobile)) return res.status(400).send({ status: false, msg: "mobile must be required." })
+        if (!isValid(mobile)) return res.status(400).send({ status: false, message: "mobile must be required." })
         mobile=data.mobile.trim()
-        if (!isValidMobile(mobile)) return res.status(400).send({ status: false, msg: "Please enter valid mobile." })
+        if (!isValidMobile(mobile)) return res.status(400).send({ status: false, message: "Please enter valid mobile." })
 
         let doc = await internModel.findOne({ mobile: mobile, isDeleted: false })
-        if (doc) return res.status(400).send({ status: false, msg: "mobile is already registered." })
+        if (doc) return res.status(400).send({ status: false, message: "mobile is already registered." })
 
-        if (!isValid(email)) return res.status(400).send({ status: false, msg: "email must be required." })
+        if (!isValid(email)) return res.status(400).send({ status: false, message: "email must be required." })
         email=data.email.trim()
-        if (!validateEmail(email)) return res.status(400).send({ status: false, msg: "Please enter valid email." })
+        if (!validateEmail(email)) return res.status(400).send({ status: false, message: "Please enter valid email." })
         
         let emaildoc = await internModel.findOne({ email: email, isDeleted: false })
-        if (emaildoc) return res.status(400).send({ status: false, msg: "email is already registered." })
+        if (emaildoc) return res.status(400).send({ status: false, message: "email is already registered." })
 
-        if (!isValid(collegeName)) return res.status(400).send({ status: false, msg: "collegeName must be required." })
+        if (!isValid(collegeName)) return res.status(400).send({ status: false, message: "collegeName must be required." })
         if (!isValidName(collegeName)) return res.status(400).send({ status: false, message: "collegeName can contain only letters" })
         
         let collegeDetails = await collegeModel.findOne({ name: collegeName.toLowerCase(), isDeleted: false })
-        if (!collegeDetails) return res.status(404).send({ status: false, msg: "college not found..Please try with another college Name." })
+        if (!collegeDetails) return res.status(404).send({ status: false, message: "college not found..Please try with another college Name." })
         data["collegeId"] = collegeDetails["_id"]
 
         let result = await internModel.create(data)
         return res.status(201).send({ status: true, data: result })
     }
     catch (err) {
-        return res.status(500).send({ status: false, msg: err.message })
+        return res.status(500).send({ status: false, message: err.message })
     }
 
 }
